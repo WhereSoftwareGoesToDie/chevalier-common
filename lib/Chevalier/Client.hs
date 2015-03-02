477cf4037b00a2112d88159926f3ccb409c23235
Sharif Olorin <sio@tesser.org>
2015-03-02 22:36:33 +0000

    Add getAddresses' taking multiple tags for more flexible querying
    Also add some comments.

--- lib/Chevalier/Client.hs
+++ lib/Chevalier/Client.hs
@@ -1,10 +1,14 @@
-module Chevalier.Client
-     ( getSourceDict, getAddresses )
-where
+module Chevalier.Client(
+    getSourceDict,
+    getAddresses,
+    getAddresses'
+) where
 
 import           Control.Monad.Trans
+import           Data.Bifunctor
 import           Data.Either
 import           Data.List
+import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Data.Text.Encoding         (encodeUtf8)
 import           Network.URI
@@ -19,9 +23,18 @@ getSourceDict uri org addr = do
   resp <- runChevalier uri org $ buildRequestFromAddress addr
   return $ fmap snd $ find ((==addr) . fst) resp
 
+-- | Take one key-value pair and return matched addresses and
+--   sourcedicts.
 getAddresses :: MonadIO m => URI -> Origin -> (String, String) -> m [(Address, SourceDict)]
-getAddresses uri org (k,v) =
-  runChevalier uri org $ buildRequestFromPairs [(T.pack k, T.pack v)]
+getAddresses uri org = getAddresses' uri org . (: [])
+
+-- | Take multiple key-value pairs and return matchec addresses and
+--   sourcedicts.
+getAddresses' :: MonadIO m => URI -> Origin -> [(String, String)] -> m [(Address, SourceDict)]
+getAddresses' uri org tags =
+  runChevalier uri org $ buildRequestFromPairs $ packTags tags
+ where
+  packTags = map (bimap T.pack T.pack)
 
 runChevalier :: MonadIO m => URI -> Origin -> SourceRequest -> m [(Address, SourceDict)]
 runChevalier uri origin req = do
